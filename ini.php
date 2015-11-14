0f2107ed907e0b4628211daa6073881d5a4cfab1
Zyr <zyrius@live.com>
2015-11-14 17:38:00 +0100

    Reordered configuration settings

--- ini.php
+++ ini.php
@@ -10,12 +10,10 @@ defined('DIAMONDMVC') or die();
 
 DEBUG_MODE=1
 VERBOSE_LOGGING=1 ; If set (1), a short stacktrace of by default 5 calls will be logged along.
-
-DATABASE=DEFAULT
-SESSION_TIMEOUT=15 ; in minutes
-DEFAULT_CACHE_LIFETIME=90000 ; 1 day in milliseconds
-
 LOG_SEVERITY=1 ; Log messages with a priority higher than this will be logged.
+
+SESSION_TIMEOUT=15 ; in minutes
+DEFAULT_CACHE_LIFETIME=90000 ; ~1 day in seconds
 
 WEBSITE_TITLE='DiamondMVC Demo Site' ; Title of the website, as usually displayed in the <title> element and/or as website header.
 
@@ -23,9 +21,8 @@ DEFAULT_CONTROLLER=ControllerFrontpage ; Default controller to route to if the r
 DEFAULT_CONTROLLER_TITLE='DiamondMVC' ; Either empty this or change it to a more appropriate title.
 DEFAULT_LOGIN_REDIRECT='/user' ; Web URL relative to system root to redirect the user to after login
 
-; Database column enforcement rules
-
-DBO_ENFORCE_COL_DELETED=true
+DATABASE=DEFAULT
+DBO_ENFORCE_COL_DELETED=1 ; Whether to enforce the database column `deleted` for all tables
 
 [DATABASE.DEFAULT]
 HOST=127.0.0.1
