ed35eeb41115a1bf837263e129d81b59738b0861
Zyr <zyrius@live.com>
2015-11-14 04:59:43 +0100

    Updated version number stored in DIAMONDMVC constant and changed initial installation approach

--- index.php
+++ index.php
@@ -7,25 +7,18 @@
  * 
  * The main engine of the DiamondMVC which instructs the controllers and renders the views.
  */
-define('DIAMONDMVC', '0.2');
+define('DIAMONDMVC', '1.1.3');
 define('DIAMONDMVC_ROOT', dirname(__FILE__));
-if ($_SERVER['SERVER_PROTOCOL'] == 'https' || $_SERVER['SERVER_PORT'] == '443')
-{
-    $chelpurl = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
-}
-else
-{
-    $chelpurl = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
-}
-$chelpurl = str_replace('\\', '/', $chelpurl);
-if (substr($chelpurl, -1) == '/')
-{
-    $chelpurl = substr($chelpurl, 0, strlen($chelpurl) - 1);
+
+$https = $_SERVER['SERVER_PROTOCOL'] === 'https' or $_SERVER['SERVER_PORT'] == 443;
+$tmp   = ($https ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+$tmp   = str_replace('\\', '/', $tmp);
+if( substr($tmp, -1) === '/' ) {
+    $tmp = substr($tmp, 0, strlen($tmp) - 1);
 }
 
-define('DIAMONDMVC_URL', $chelpurl, true);
-unset($chelpurl);
-// define('DIAMONDMVC_URL',  str_replace('\\', '/', str_replace(dirname(DIAMONDMVC_ROOT), '', DIAMONDMVC_ROOT)));
+define('DIAMONDMVC_URL', $tmp, true);
+unset($tmp);
 
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
@@ -41,7 +34,7 @@ require_once(DIAMONDMVC_ROOT . '/lib/autoload.php');
 session_start();
 
 if( !DiamondMVC::instance()->isInstalled() ) {
-	DiamondMVC::instance()->install();
+	redirect(DIAMONDMVC_URL . '/firstinstallation');
 }
 else {
 	DiamondMVC::instance()->run();
